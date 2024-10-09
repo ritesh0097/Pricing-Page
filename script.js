3633adb5883b8107ea34897fdc5f5fa3fa2cad17
Ritesh Kumar <ritdev0097@gmail.com>
2024-10-09 13:00:54 +0530

    files added

---
+++ script.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let menuBtn = document.getElementById("menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
